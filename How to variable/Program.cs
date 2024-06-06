b122dd7e61275cea26cff8561278573de429cfb8
Adrian Grøn <adriangron7@gmail.com>
2024-06-06 14:33:50 +0200

    Add project files.

---
+++ How to variable/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace How_to_variable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num01;
+            int num02;
+            int num03;
+
+            Console.WriteLine("Input a number");
+           
+            num01 = Convert.ToInt32 (Console.ReadLine());
+
+            Console.WriteLine("Input second number");
+
+            num02 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input the final number");
+
+            num03 = Convert.ToInt32(Console.ReadLine());
+
+            int result = (num01 + num02 + num03) / 3;
+            Console.WriteLine("The avarage of the numbers is " + result);
+
+        }
+    }
+}
